e19486d60e8ee2266fac6de823c4ea5f2a24a76b
babudivyansh <divyansh.verma525@gmail.com>
2024-02-01 16:14:41 +0530

    [Divyansh Babu]Refactor: Adding login route , password hashing and jwt auth in user login.

--- Routes/user.py
+++ Routes/user.py
@@ -1,10 +1,14 @@
 from sanic import Blueprint, response
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import sessionmaker
 from Core.Models import User, async_engine
-from Core.Schema import UserValidator
+from Core.Schema import UserValidator, UserLogin
 from sanic_ext import validate, openapi
+from Core.Utils import Hasher, JWT
 
+
+jwt_handler = JWT()
 app = Blueprint('user')
 
 async_session = sessionmaker(async_engine, class_=AsyncSession)
@@ -46,6 +50,7 @@ async def register_user(request, body):
     """
     try:
         user_validator = UserValidator.model_dump(body)
+        user_validator['password'] = Hasher.get_hash_password(user_validator['password'])
         user = User(**user_validator)
         async with async_session() as session:
             session.add(user)
@@ -54,3 +59,27 @@ async def register_user(request, body):
         return response.json({"message": "User registered successfully", "user_id": user.id}, status=201)
     except Exception as e:
         return response.json({"message": "Failed to register user", "error": str(e)}, status=500)
+
+
+@app.post('/login/')
+@openapi.definition(body={'application/json': UserLogin.model_json_schema()})
+@validate(json=UserLogin)
+async def login(request, body: UserLogin):
+    """
+    Endpoint to authenticate a user.
+
+    :param request: Sanic request object.
+    :param body: UserLogin schema containing username and password.
+    :return: JSON response indicating success or failure of authentication.
+    """
+    try:
+        async with async_session() as session:
+            user = await session.execute(select(User).filter(User.username == body.username))
+            user = user.scalars().first()
+            if not user or not Hasher.verify_password(body.password, user.password):
+                return response.json({"message": "Invalid username or password"}, status=401)
+            token = jwt_handler.jwt_encode({'user_id': user.id})
+            return response.json({"message": "Login successful", "user_id": user.id, 'access_token': token}, status=200)
+
+    except Exception as e:
+        return response.json({"message": "Failed to authenticate user", "error": str(e)}, status=500)
