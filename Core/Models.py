de4bd4bbf87be1610c6a0aa8f5e667ee3409ccf5
babudivyansh <divyansh.verma525@gmail.com>
2024-02-01 12:33:19 +0530

    [Divyansh Babu]Add: Adding the Models for the user registration and user Login.

---
+++ Core/Models.py
@@ -0,0 +1,21 @@
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, String, BigInteger, Boolean, DateTime, ForeignKey, Table
+from Core.Settings import DATABASE_NAME, DATABASE_PASSWORD
+
+async_engine = create_async_engine(f"postgresql+asyncpg://postgres:{DATABASE_PASSWORD}@localhost:5432/{DATABASE_NAME}", echo=True, future=True)
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    id = Column(BigInteger, primary_key=True, index=True)
+    username = Column(String(250), unique=True)
+    password = Column(String(250))
+    first_name = Column(String(150))
+    last_name = Column(String(150))
+    email = Column(String(150))
+    phone = Column(BigInteger)
+    location = Column(String(150))
+    is_superuser = Column(Boolean, default=False)
